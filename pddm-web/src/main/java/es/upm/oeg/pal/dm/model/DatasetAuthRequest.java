865ed600418db30d40234098d6a944d7ea18ee50
Víctor Rodríguez Doncel <victorr@ac.upc.edu>
2021-06-28 10:48:52 +0200

    The method has been better commetned

--- pddm-web/src/main/java/es/upm/oeg/pal/dm/model/DatasetAuthRequest.java
+++ pddm-web/src/main/java/es/upm/oeg/pal/dm/model/DatasetAuthRequest.java
@@ -1,6 +1,7 @@
 package es.upm.oeg.pal.dm.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * Request for authorization from DSpaces. This should be represented with a
@@ -10,10 +11,19 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class DatasetAuthRequest {
 
+    @ApiModelProperty(value = "URI of a rdf license", example = "http://purl.org/NET/rdflicense/ms-c-nored-ff")
     private String license;
+
+    @ApiModelProperty(value = "Examples: research, commercial", example = "research")
     private String institution;
+
+    @ApiModelProperty(value = "Examples: research, commercial", example = "research")
     private String purpose;
+
+    @ApiModelProperty(value = "Date in ISO format", example = "2020-06-21")
     private String initDate;
+
+    @ApiModelProperty(value = "Date in ISO format", example = "2020-06-21")
     private String endDate;
 
     public String getLicense() {
